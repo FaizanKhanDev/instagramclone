3ba57200ba0480845ff9abca8f81ddb37184628b
faizankhandev <faizan480khan@gmail.com>
2024-07-19 08:21:47 -0700

    ADDED Storage folder

---
+++ src/storage/database/setting.js
@@ -0,0 +1,54 @@
+const setting = [
+  {
+    id: 1,
+    icon: require('../images/invite.png'),
+    name: 'Follow Friends and Invite',
+  },
+  {
+    id: 2,
+    icon: require('../images/bell.png'),
+    name: 'Notifications',
+  },
+  {
+    id: 3,
+    icon: require('../images/padlock.png'),
+    name: 'Privacy',
+  },
+  {
+    id: 4,
+    icon: require('../images/friend.png'),
+    name: 'Surveillance',
+  },
+  {
+    id: 5,
+    icon: require('../images/security.png'),
+    name: 'Security',
+  },
+  {
+    id: 6,
+    icon: require('../images/megaphone.png'),
+    name: 'Advertisements',
+  },
+  {
+    id: 7,
+    icon: require('../images/user.png'),
+    name: 'Account',
+  },
+  {
+    id: 8,
+    icon: require('../images/lifesaver.png'),
+    name: 'Help',
+  },
+  {
+    id: 9,
+    icon: require('../images/about.png'),
+    name: 'About',
+  },
+  {
+    id: 10,
+    icon: require('../images/color-palette.png'),
+    name: 'Theme',
+  },
+];
+
+export default setting;
