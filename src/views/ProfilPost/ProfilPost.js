61b318f191b607086c91babb2bace9dfa4282117
faizankhandev <faizan480khan@gmail.com>
2024-07-20 10:50:25 -0700

    FIXING: Profile Post list

--- src/views/ProfilPost/ProfilPost.js
+++ src/views/ProfilPost/ProfilPost.js
@@ -1,22 +1,44 @@
 import React from 'react';
-import {Image, TouchableOpacity} from 'react-native';
-
+import { Image, TouchableOpacity, ScrollView, View, StyleSheet } from 'react-native';
 import Container from '../../components/Container/Container';
 
-const ProfilPost = ({navigation}) => {
+const ProfilPost = ({ navigation }) => {
   return (
     <Container>
-      <TouchableOpacity onPress={() => navigation.navigate('SinglePost')}>
-        <Image
-          source={require('../../storage/images/post.jpg')}
-          style={{
-            width: '33%',
-            height: 135,
-            marginTop: 5,
-          }}
-        />
-      </TouchableOpacity>
+      <ScrollView>
+        <View style={styles.imageRow}>
+          {[...Array(6)].map((_, index) => (
+            <TouchableOpacity
+              key={index}
+              style={styles.imageContainer}
+              onPress={() => navigation.navigate('SinglePost')}
+            >
+              <Image
+                style={styles.image}
+                source={require('../../storage/images/post.jpg')}
+              />
+            </TouchableOpacity>
+          ))}
+        </View>
+      </ScrollView>
     </Container>
   );
 };
+
+const styles = StyleSheet.create({
+  imageRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    flexWrap: 'wrap',
+  },
+  image: {
+    width: '100%',
+    height: 135,
+  },
+  imageContainer: {
+    width: '33%',
+    height: 135,
+  },
+});
+
 export default ProfilPost;
